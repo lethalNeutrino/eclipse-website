853174eb2de96ce26dfc16ef40b8031f072e7d87
Andrew <liu.andrew@berkeley.edu>
2024-04-16 21:09:21 -0700

    initial commit

---
+++ content/final.md
@@ -0,0 +1,5 @@
++++
+title = "Eclipse Simulation Checkpoint"
+template = "index.html"
++++
+
---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in rec {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+			zola
+        ];
+		shellHook = "zola serve";
+      };
+    }
+  );
+}
